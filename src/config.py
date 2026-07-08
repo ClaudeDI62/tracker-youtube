1a25cd8dce42369b8a564bc59674b0bc08753e7e
ClaudeDI62 <299022879+ClaudeDI62@users.noreply.github.com>
2026-07-08 22:30:57 +0200

    Aggiunti 5 nuovi canali YouTube + guida aggiornata v3

--- src/config.py
+++ src/config.py
@@ -30,6 +30,41 @@ PILOT_CHANNELS = [
         "language": "DE",
         "market_default": "Europe",
     },
+    {
+        "name": "Ticker Symbol: YOU",
+        "url": "https://www.youtube.com/@TickerSymbolYOU",
+        "channel_id": "UCORX3Cl7ByidjEgzSCgv9Yw",
+        "language": "EN",
+        "market_default": "USA",
+    },
+    {
+        "name": "Millionaires Investment Secrets",
+        "url": "https://www.youtube.com/channel/UCESh5daDcPqvG0QhbSdzIzw",
+        "channel_id": "UCESh5daDcPqvG0QhbSdzIzw",
+        "language": "EN",
+        "market_default": "USA",
+    },
+    {
+        "name": "Honeystocks Active Investing",
+        "url": "https://www.youtube.com/channel/UCmNwLkHnslfzEtO2S16vmcQ",
+        "channel_id": "UCmNwLkHnslfzEtO2S16vmcQ",
+        "language": "EN",
+        "market_default": "USA",
+    },
+    {
+        "name": "Clive Thompson",
+        "url": "https://www.youtube.com/@clivethompson-jc9my",
+        "channel_id": "UCrlFUp4OtXJSiDfiPMFnk3A",
+        "language": "EN",
+        "market_default": "Globale",
+    },
+    {
+        "name": "BWB - Business With Brian",
+        "url": "https://www.youtube.com/@BusinessWithBrian",
+        "channel_id": "UCyqlbzLoYtpqDXwRI9Yh5LA",
+        "language": "EN",
+        "market_default": "USA",
+    },
 ]
 
 RSS_TEMPLATE = "https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
